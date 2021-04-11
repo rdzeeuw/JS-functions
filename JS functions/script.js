37db6d9a598985e1ea9bd600ea5419ab690e08b7
rdzeeuw <77979140+rdzeeuw@users.noreply.github.com>
2021-04-11 21:53:56 +0200

    Add files via upload

---
+++ JS functions/script.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+    console.log("1. Make dough");
+    console.log("2. Add tomatosauce and spread it out");
+    console.log("3. Cover with mushrooms, bacon and peppers");
+    console.log("4. Sprinkle with cheese");
+    console.log("5. Put in oven and bake for 12 minutes");
+};
+
+const makeSushi = function () {
+    console.log("1. Cook rice");
+    console.log("2. Spread out rice on the seaweed");
+    console.log("3. Put crab and avocado on top of the rice");
+    console.log("4. Roll the seaweed into a roll");
+    console.log("5. Cut the roll into 1.5 cm slices");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
